01bc7c6c84ada328fe444d87f232882170d32a49
Nikita S Patil <nikitaspatil311@gmail.com>
2023-09-04 12:53:48 +0200

    add player functionality

--- dist/index.242b51c6.js
+++ dist/index.242b51c6.js
@@ -25,7 +25,7 @@ function runGame(e) {
 }
 function checkWinner() {
     const boxes = getBoxes();
-    return boxes[1] === boxes[2] && boxes[2] === boxes[3] && boxes[1] !== "" || boxes[4] === boxes[5] && boxes[5] === boxes[6] && boxes[4] !== "" || boxes[7] === boxes[8] && boxes[8] === boxes[9] && boxes[7] !== "" || boxes[4] === boxes[5] && boxes[5] === boxes[6] && boxes[4] !== "";
+    return boxes[1] === boxes[2] && boxes[2] === boxes[3] && boxes[1] !== "" || boxes[4] === boxes[5] && boxes[5] === boxes[6] && boxes[4] !== "" || boxes[7] === boxes[8] && boxes[8] === boxes[9] && boxes[7] !== "" || boxes[1] === boxes[5] && boxes[5] === boxes[9] && boxes[1] !== "" || boxes[3] === boxes[5] && boxes[5] === boxes[7] && boxes[3] !== "" || boxes[2] === boxes[5] && boxes[5] === boxes[8] && boxes[2] !== "" || boxes[1] === boxes[4] && boxes[4] === boxes[7] && boxes[1] !== "" || boxes[3] === boxes[6] && boxes[6] === boxes[9] && boxes[3] !== "";
 }
 function getBoxes() {
     const boxesContent = [];
