0f24cd4067ca2e4b57b6ff37de60404f86df2274
Nikita S Patil <nikitaspatil311@gmail.com>
2023-09-04 12:20:24 +0200

    add player functionality

--- dist/index.242b51c6.js
+++ dist/index.242b51c6.js
@@ -1,6 +1,12 @@
 function listenBoard() {
     board.addEventListener("click", runGame);
 }
+let turn = "X";
+// const TURN = {
+//   X: "X",
+//   O: "X",
+//   EMPTY: " ",
+// };
 const board = document.querySelector(".game-container");
 function main() {
     createBoard();
@@ -12,6 +18,12 @@ function runGame(e) {
     if (boxId === null) return;
     const box = document.querySelector(`#${boxId}`);
     if (box === null || box.textContent !== "") return;
+    box.textContent = turn;
+    switchPlayer(turn);
+}
+function switchPlayer(turn) {
+    if (turn === "X") turn = "O";
+    else turn = "X";
 }
 function createBoard() {
     for(let i = 1; i <= 9; i++)makeBox(i);
--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
 function listenBoard(): void {
   board.addEventListener("click", runGame);
 }
+
+type Turn = "X" | "O" | " ";
+let turn: Turn = "X";
+
+// const TURN = {
+//   X: "X",
+//   O: "X",
+//   EMPTY: " ",
+// };
 const board = document.querySelector(".game-container") as HTMLElement;
 function main(): void {
   createBoard();
@@ -13,6 +22,16 @@ function runGame(e: Event): void {
   if (boxId === null) return;
   const box: HTMLElement | null = document.querySelector(`#${boxId}`);
   if (box === null || box.textContent !== "") return;
+  box.textContent = turn;
+  switchPlayer(turn);
+}
+
+function switchPlayer(turn: Turn): void {
+  if (turn === "X") {
+    turn = "O";
+  } else {
+    turn = "X";
+  }
 }
 
 function createBoard(): void {
